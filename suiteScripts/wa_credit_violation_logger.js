3276d69df76d06a18fc6e9894b9ab9f374beaf1f
Cornelius Maxwell <156154530+corneliusmaxwell2015@users.noreply.github.com>
2026-08-15 14:40:21 -0500

    Add credit violation logging functionality

--- suiteScripts/wa_credit_violation_logger.js
+++ suiteScripts/wa_credit_violation_logger.js
@@ -1,1 +1,18 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType WorkflowActionScript
+ */
+define(['N/record'], (record) => {
+
+    const onAction = (context) => {
+        const so = context.newRecord;
+        const log = record.create({ type: 'customrecord_creditlog' });
+
+        log.setValue('custrecord_salesorder', so.id);
+        log.setValue('custrecord_message', 'Credit violation logged.');
+        log.save();
+    };
+
+    return { onAction };
+});
 
