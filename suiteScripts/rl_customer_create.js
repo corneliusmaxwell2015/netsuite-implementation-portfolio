e6eebc84ee50529082c4fe8c5d6017731de8be76
Cornelius Maxwell <156154530+corneliusmaxwell2015@users.noreply.github.com>
2026-08-15 14:38:27 -0500

    Create RESTlet for customer creation

--- suiteScripts/rl_customer_create.js
+++ suiteScripts/rl_customer_create.js
@@ -1,1 +1,16 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Restlet
+ */
+define(['N/record'], (record) => {
+
+    const post = (data) => {
+        const customer = record.create({ type: 'customer' });
+        customer.setValue('companyname', data.companyname);
+        customer.setValue('email', data.email);
+        return { id: customer.save() };
+    };
+
+    return { post };
+});
 
